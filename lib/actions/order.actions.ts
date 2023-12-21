be8b40c9068dd8788c929ead168b54431672266f
Ashutosh Bansal <ashutosh.bansal98@thehutgroup.com>
2023-12-21 16:22:55 +0000

    fix catch error

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -46,6 +46,6 @@ export const createOrder = async (order: CreateOrderParams) => {
     });
     return JSON.parse(JSON.stringify(newOrder));
   } catch (error) {
-    handleError();
+    handleError(error);
   }
 };
