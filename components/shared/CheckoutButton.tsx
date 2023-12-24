e23a6bee7a1234f69931080db96800137de8fc72
Ashutosh Bansal <ashutosh.bansal98@thehutgroup.com>
2023-12-24 14:00:03 +0000

    fix

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -13,7 +13,6 @@ interface Props {
 
 const CheckoutButton: React.FC<Props> = ({ event }) => {
   const { user } = useUser();
-  if (!user) return null;
 
   useEffect(() => {
     setTimeout(async () => {
@@ -25,30 +24,31 @@ const CheckoutButton: React.FC<Props> = ({ event }) => {
 
   const hasEventFinished = new Date(event.endDateTime) < new Date();
 
-  const renderContent = () => {
-    if (!user?.publicMetadata.userId) return <Spinner />;
-    return (
-      <>
-        {hasEventFinished ? (
-          <p className="p-2 text-red-500">
-            Sorry, tickets are not longer available
-          </p>
-        ) : (
-          <>
-            <SignedOut>
-              <Button asChild size="lg" className="button rounded-full">
-                <Link href="/sign-in">Get Tickets</Link>
-              </Button>
-            </SignedOut>
-            <SignedIn>
+  return (
+    <div className="flex items-center gap-3">
+      {hasEventFinished ? (
+        <p className="p-2 text-red-400">
+          Sorry, tickets are no longer available.
+        </p>
+      ) : (
+        <>
+          <SignedOut>
+            <Button asChild className="button rounded-full" size="lg">
+              <Link href="/sign-in">Get Tickets</Link>
+            </Button>
+          </SignedOut>
+
+          <SignedIn>
+            {loggedInUserId ? (
               <Checkout event={event} loggedInUserId={loggedInUserId} />
-            </SignedIn>
-          </>
-        )}
-      </>
-    );
-  };
-  return renderContent();
+            ) : (
+              <Spinner />
+            )}
+          </SignedIn>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default CheckoutButton;
